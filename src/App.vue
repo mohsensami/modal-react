b3ab4a195870daa3543b93151e8702eb25d17147
MohsenSami <mohsensami88@gmail.com>
2023-01-11 22:20:42 +0330

    save todo object to localstorage and get them

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
             <div class="">
               <h2 class="text-center text-2xl py-4">MY TODO LIST</h2>
               <div class="flex">
-                <input class="w-full py-2 px-4" type="text" name="content" id="content" placeholder="e.g. make a ToDo" v-model="input_content" />
+                <input class="w-full py-2 px-4 border-gray-200 border focus:outline-none" type="text" name="content" id="content" placeholder="e.g. make a ToDo" v-model="input_content" />
                 <input class="bg-gray-700 text-white cursor-pointer py-1 px-8" type="submit" value="Add todo" />
               </div>
             </div>
@@ -19,10 +19,9 @@
 
         <div class="col-span-12 bg-gray-100">
           <div class="mx-auto flex flex-col gap-1" id="todo-list">
-            <div class="bg-gray-500 w-full flex text-white text-center items-center" v-for="todo in todos" :key="todo.id" :class="{'completed': todo.done}" >
-
-              <div @click="checkTodo(todo.id)" class="todo-content flex-grow cursor-pointer">
-                {{todo.content}}
+            <div class="bg-gray-500 w-full flex text-white text-center items-center" v-for="todo in todos" :key="todo.id" :class="{ completed: todo.done }">
+              <div @click="checkTodo(todo.id)" class="todo-content flex-grow cursor-pointer text-xl">
+                {{ todo.content }}
               </div>
 
               <div class="actions">
@@ -42,39 +41,30 @@
 
 <script setup lang="ts">
 import { ref, onMounted, computed, watch, reactive } from "vue";
-import {todoType} from './types/Todo'
+import { todoType } from "./types/Todo";
 
 const name = ref<string | number>("");
 
 const input_content = ref("");
 
-
-
-// const todos_asc = computed(() =>
-//   todos.value.sort((a, b) => {
-  //     return a.createdAt - b.createdAt;
-//   })
-// );
+const todos = reactive<todoType[]>([
+  { id: 1, content: "watch TV", done: false },
+  { id: 2, content: "playing football", done: false },
+  { id: 3, content: "Learning javascript", done: false },
+]);
 
 watch(name, (newVal: string) => {
-    localStorage.setItem("name", newVal);
+  localStorage.setItem("name", newVal);
 });
 
-// watch(
-  //   todos,
-//   (newVal) => {
-  //     localStorage.setItem("todos", JSON.stringify(newVal));
-//   },
-//   {
-  //     deep: true,
-//   }
-// );
 
-const todos = reactive<todoType[]>([
-  {id: 1, content: 'watch TV', done: false},
-  {id: 2, content: 'playing football', done: false},
-  {id: 3, content: 'Learning javascript', done: false},
-]);
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  }
+);
+
 
 const addTodo = () => {
   if (input_content.value.trim() === "") {
@@ -86,25 +76,25 @@ const addTodo = () => {
     content: input_content.value,
     done: false,
   });
-  input_content.value = ""
+  input_content.value = "";
 };
 
-const checkTodo = (todoId:number) => {
-  todos.map((todo)=> {
+const checkTodo = (todoId: number) => {
+  todos.map((todo) => {
     if (todo.id == todoId) {
-      todo.done = !todo.done
+      todo.done = !todo.done;
     }
-  })
+  });
 };
 
-const removeTodo = (todoItem:todoType) => {
-  const todoIndex = todos.findIndex(todo => todo === todoItem);
-  todos.splice(todoIndex, 1)
+const removeTodo = (todoItem: todoType) => {
+  const todoIndex = todos.findIndex((todo) => todo === todoItem);
+  todos.splice(todoIndex, 1);
 };
 
 onMounted(() => {
   name.value = localStorage.getItem("name") || "";
-  // todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+  Object.assign( todos , JSON.parse(localStorage.getItem("todos")));
 });
 </script>
 
