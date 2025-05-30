a8a5a7d2671e0afdbae99fd277d4035be596de90
mohsensami <mohsensami18727@gmail.com>
2025-05-30 22:38:42 +0330

    size added

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
-import { Modal } from './lib/Modal';
+import React, { useState } from "react";
+import { Modal } from "./lib/Modal";
 // import './App.css';
 
 const App: React.FC = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    return (
-        <div className="App">
-            <h1>My App</h1>
-            <button onClick={openModal}>Open Modal</button>
-            <Modal
-                isOpen={isModalOpen}
-                title="My Modal"
-                onClose={closeModal}
-                footer={<button onClick={closeModal}>Close</button>}
-                animationName="bounceInDown"
-                animateDuration="0.9"
-            >
-                <p>This is the modal content.</p>
-            </Modal>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>My App</h1>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={isModalOpen}
+        title="My Modal"
+        onClose={closeModal}
+        footer={<button onClick={closeModal}>Close</button>}
+        animationName="bounceInDown"
+        animateDuration="0.9"
+        size="lg"
+      >
+        <p>This is the modal content.</p>
+      </Modal>
+    </div>
+  );
 };
 
 export default App;
