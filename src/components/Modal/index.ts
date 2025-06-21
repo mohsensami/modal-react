0ec67d4d00ab895141924d20563fc133ebe55481
mohsensami <mohsensami18727@gmail.com>
2025-06-21 21:26:29 +0330

    update vite config file 2

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,1 +1,2 @@
-export { Modal } from "./Modal";
+import { Modal } from "./Modal";
+export default Modal;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { Modal } from "./components/Modal";
+export { default as Modal } from "./components/Modal";
