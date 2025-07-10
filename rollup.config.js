9aa75fa5e0dc4241351f80e014f6ec67d269924a
mohsensami <mohsensami18727@gmail.com>
2025-07-10 22:47:12 +0330

    Readme file added

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
+import babel from "@rollup/plugin-babel";
 
 export default {
   input: "src/Modal.tsx",
@@ -26,5 +27,12 @@ export default {
     postcss({
       modules: true,
     }),
+    babel({
+      babelHelpers: "bundled",
+      presets: [["@babel/preset-react", { runtime: "automatic" }]],
+    }),
   ],
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
 };
